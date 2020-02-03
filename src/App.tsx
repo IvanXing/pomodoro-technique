b6207bc3f40b45d762201f941568a81a57a4b759
xingyifan <xingyifan10@qq.com>
2020-02-03 11:36:39 +0800

    init file

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Button } from 'antd';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Button type="primary">Primary</Button>
+    <Button>Default</Button>
+    <Button type="dashed">Dashed</Button>
+    <Button type="danger">Danger</Button>
+    <Button type="link">Link</Button>
+    </div>
+  );
+}
+
+export default App;
